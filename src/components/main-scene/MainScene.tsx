dea3e8e0ddb6a9bd51ddb199eac9140698561627
Andrii Ivaniv <unixvb@gmail.com>
2024-03-09 13:13:37 +0200

    scroll to bottom added

--- src/components/main-scene/MainScene.tsx
+++ src/components/main-scene/MainScene.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Player} from "@lottiefiles/react-lottie-player";
 import styles from "./MainScene.module.css";
 import {Trail} from "./trail/Trail";
+import {useCursorVariant} from "../../hooks/cursor-variant/cursor-variant.hook";
 
 export const MainScene = () => {
+    const {onMouseEnter, onMouseLeave} = useCursorVariant();
+
+    const bottomRef = useRef<HTMLDivElement | null>(null);
+
+    const handleClick = () => bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+
     return (
-        <div className={styles.container}>
-            <div/>
-            <Trail/>
-            <Player src={'./scroll down.json'}
-                    className={styles.scroll_down}
-                    autoplay={true}
-                    loop={true}/>
-        </div>
+        <>
+            <div className={styles.container}>
+                <div/>
+                <Trail/>
+                <div onMouseEnter={onMouseEnter}
+                     onMouseLeave={onMouseLeave}
+                     onClick={handleClick}>
+                    <Player src={'./scroll down.json'}
+                            className={styles.scroll_down}
+                            autoplay={true}
+                            loop={true}/>
+                </div>
+            </div>
+            <div ref={bottomRef}/>
+        </>
     );
 };
